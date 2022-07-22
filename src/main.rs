e28ccc655ecc06512769ff121a8f4003afee92d7
martintc <kj4ntv@gmail.com>
2022-07-21 21:07:59 -0700

    Test out sending bytes of data over serial

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,72 @@
-use std::time::Duration;
 use std::str;
+use std::time::Duration;
 
 use serialport::DataBits;
+use serialport::SerialPort;
 
-fn main() -> ! {
+fn communication(port: &mut Box<dyn SerialPort>) {
+    port.write("a".as_bytes()).expect("Write failed");
+    let mut has_received: bool = false;
+    while !has_received {
+        let bytes: u32 = match port.bytes_to_read() {
+            Ok(b) => b,
+            Err(e) => panic!("An error has occured attempting to received: {}", e),
+        };
+        if bytes == 0 {
+            continue;
+        }
+        let mut buffer: Vec<u8> = vec![0; bytes.try_into().unwrap()];
+        port.read(buffer.as_mut_slice()).expect("An error occured trying to read in messagee");
+        let recv_data = str::from_utf8(&buffer);
+        match recv_data {
+            Ok(s) => {
+                println!("Message received: {}", s);
+                has_received = true;
+            },
+            Err(e) => panic!("An error occured writing received data: {}", e),
+        }
+    }
+}
+
+fn wait_for_device(port: &mut Box<dyn SerialPort>) {
+    let mut data: String = String::from("");
+    while data != "Hello" {
+        let bytes: u32 = match port.bytes_to_read() {
+            Ok(b) => b,
+            Err(e) => panic!("An errored occured: {}", e),
+        };
+        if bytes == 0 {
+            continue;
+        }
+        let mut buffer: Vec<u8> = vec![0; bytes.try_into().unwrap()];
+        let _ = port.read(buffer.as_mut_slice());
+        let recv_data = str::from_utf8(&buffer);
+        match recv_data {
+            Ok(s) => {
+                println!("{}", s);
+                data = s.to_string();
+            },
+            Err(e) => panic!("An errored occured: {}", e),
+        }
+    }
+}
+
+fn main() {
     let ports = serialport::available_ports().expect("No ports found!");
     for p in ports {
-	println!("{}", p.port_name);
+        println!("{}", p.port_name);
     }
     let mut port = serialport::new("/dev/tty.usbserial-AB0LBV6H", 115_200)
         .timeout(Duration::from_millis(10))
-        .open().expect("Failed to open port");
+        .open()
+        .expect("Failed to open port");
 
-    // let mut serial_buf: Vec<u8> = vec![0; 32];
-    // port.read(serial_buf.as_mut_slice()).expect("No data found!");
-    // let string = match str::from_utf8(&serial_buf) {
-    // 	Ok(s) => s,
-    // 	Err(_) => "Not able to convert data.",
-    // };
+    wait_for_device(&mut port);
 
-    // println!("{:?}", serial_buf);
-    // println!("{string}");
-    let mut total_bytes = 0;
-    // just continually receive
-    loop {
-	    let bytes: u32 = match port.bytes_to_read() {
-	        Ok(b) => b,
-	        Err(e) => {
-		        println!("Error {:?}", e);
-		        continue;
-	        }
-	    };
-
-        total_bytes += bytes;
-        if bytes == 0 {
-            //print!(".");
-            continue;
-        }
-        println!("");
-        println!("Bytes recieved: {}", bytes);
-	    let mut buffer: Vec<u8> = vec![0; bytes.try_into().unwrap()];
-    	// let size = port.read(buffer.as_mut_slice()).expect("Was not able to read data from serial port");
-        let size = port.read(buffer.as_mut_slice());
-        match size {
-            Ok(s) => {
-                println!("size: {}", s);
-            },
-            Err(e) => {
-                panic!("An error occured: {}", e);
-            }
-        };
-	    for data in buffer.iter() {
-            print!("{} ", data);
-        }
-        print!("\n");
-        let string = str::from_utf8(&buffer);
-        match string {
-            Ok(s) => println!("{}", s),
-            Err(r) => panic!("An error occured: {}", r),
-        }
-
-        println!("\nTotal bytes recv: {}", total_bytes);
+    for i in 0..5 {
+        communication(&mut port);
     }
+
+    println!("End of commnication");
+
 }
